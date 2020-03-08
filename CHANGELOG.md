52bf7fcc99666047214e29582e0653ffc1bea090
Dieter Oberkofler <dieter.oberkofler@gmail.com>
2020-03-08 19:43:07 +0100

    Add API documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,11 +6,21 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+## [?.?.?] - ????-??-??
+
+### Added
+### Changed
+### Fixed
+
+
+## [0.0.2] - 2020-03-08
+
+### Added
+- API documentation.
+- Unit tests with a coverage of about 95%.
+
+
 ## [0.0.1] - 2020-03-06
 
 ### Added
 - Initial release.
-
-### Changed
-
-### Fixed
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,13 @@
+/**
+*	Task type definition.
+*
+*	@typedef {Object} taskType
+*	@property {string} id - The unique id of the task.
+*	@property {string} text - The title of the task.
+*	@property {Date} startDate - The start date of the task.
+*	@property {number} days - The duration in days of the task.
+*	@property {Array<number>} [dependencies] - The optional list of tasks this tasks depends upon.
+*/
 export declare type taskType = {
 	id: number;
 	text: string;
@@ -6,19 +16,73 @@ export declare type taskType = {
 	dependencies?: Array<number>;
 };
 
+/**
+*	Tree column type definition.
+*
+*	@typedef {Object} headerColumnType
+*	@property {string} id - The unique id of the column.
+*	@property {string} title - The title of the column.
+*	@property {string} [width] - The optional width in pixel of the column.
+*/
 export declare type headerColumnType = {
 	id: string;
 	title: string;
 	width?: number;
 };
 
+/**
+*	Gantt chart configuration options type definition.
+*
+*	@typedef {Object} optionsType
+*	@property {number} [treeWidthPercentage] - The percentual width of the tree.
+*	@property {Array<headerColumnType>} [columns] - The columns of the tree.
+*/
 export declare type optionsType = {
 	treeWidthPercentage?: number;
 	columns?: Array<headerColumnType>;
 };
 
+/**
+* Gantt Chart.
+*/
 export declare class GanttChart {
+	/**
+	*	Constructor.
+	*
+	*	@example const gantt = new GanttChart();
+	*/
+	constructor();
+	
+	/**
+	*	Set configuration options.
+	*
+	*	@param {optionsType} options - The configuration object.
+	*
+	*	@example gantt.setConfig({treeWidthPercentage: 50});
+	*/
 	setConfig(options: optionsType): void;
+	
+	/**
+	*	Set the tasks and dependencies.
+	*
+	*	@param {Array<taskType>} tasks - The tasks in the prject.
+	*
+	*	@example
+	*
+	*	gantt.setData([
+	*		{id: 1, text: 'Planning', startDate: new Date('2020-01-01'), days: 1},
+	*		{id: 2, text: 'Analysis', startDate: new Date('2020-01-02'), days: 1, dependencies: [1]},
+	*	]);
+	*
+	*/
+	
 	setData(tasks: Array<taskType>): void;
+	/**
+	*	Render the gantt chart in a given element.
+	*
+	*	@param {HTMLElement} container - The container element.
+	*
+	*	@example gantt.render(document.getElementById('gantt_here'));
+	*/
 	render(container: HTMLElement): void;
 }
